6316416c5f8d868fd25b44a0ce935fef2d2e2c3f
Khalid Hasan <khalidhasan@rollingglory.com>
2024-05-28 18:20:54 +0700

    Initial commit

---
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    public class TopDownCharacterController : MonoBehaviour
+    {
+        public float speed;
+
+        private Animator animator;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+
+        private void Update()
+        {
+            Vector3 dir = Vector3.zero;
+            if (Input.GetKey(KeyCode.A))
+            {
+                dir.x = -1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                dir.x = 1;
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                dir.z = 1;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                dir.z = -1;
+            }
+
+            dir.Normalize();
+
+            GetComponent<Rigidbody>().velocity = speed * dir;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 2.5D Game Explore
+ My exploration with 2.5D
